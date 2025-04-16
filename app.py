020d018e0f6e79f737f76eaceb5614d3e014996e
MZKT89 <mzhu2587@163.com>
2025-04-16 22:12:50 +0800

    修改了一点提示词

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from haiguitangAgent import TurtleSoupAgent
 
 agent = TurtleSoupAgent()
-response = agent.chat("我想玩一个关于校园的游戏")
+response = agent.chat("我想玩一个校园相关的游戏")
 memory = response["memory"]
 print(f"memory: {memory.to_messages()}") 
 print(f"外部response: {response}")
